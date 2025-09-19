fa8abb493493766c3d9b71c6a444af5aea8513c6
приложение для бухгалтерий <madiardoskali@gmail.com>
2025-09-19 19:09:13 +0500

    HttpServer query

---
+++ src/lesson23/Class_HttpServer/DayHandler.java
@@ -0,0 +1,31 @@
+package lesson23.Class_HttpServer;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.NumberFormat;
+import java.time.DayOfWeek;
+import java.time.format.TextStyle;
+import java.util.Locale;
+import java.util.Random;
+
+public class DayHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        System.out.println("Началась обработка /day запроса от клиента.");
+        DayOfWeek[] days = DayOfWeek.values();
+
+        int randomDayIndex = new Random().nextInt(days.length);
+        DayOfWeek randomDay = days[randomDayIndex];
+
+        String response = randomDay.getDisplayName(TextStyle.SHORT,Locale.ENGLISH);
+
+        httpExchange.sendResponseHeaders(200, 0);
+
+        try (OutputStream os = httpExchange.getResponseBody()) {
+            os.write(response.getBytes());
+        }
+    }
+}
---
+++ src/lesson23/Class_HttpServer/Task2.java
@@ -0,0 +1,58 @@
+package lesson23.Class_HttpServer;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Handler;
+
+public class Task2 {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/calculate",Task2::calculateHandler);
+        server.createContext("/hello",Task2::handleHistory);
+        server.start();
+    }
+    static List<String> names = new ArrayList<>();
+    static List<String> lastNames = new ArrayList<>();
+    public static void handleHistory(HttpExchange httpExchange) throws IOException{
+        String query = httpExchange.getRequestURI().getQuery();
+        String str="";
+        String [] params = str.split("&");
+        if (query != null) {
+            httpExchange.sendResponseHeaders(200, 0);
+            names.add(params.toString().split("=")[1]);
+        }
+        if (query != null) {
+            httpExchange.sendResponseHeaders(200, 0);
+            lastNames.add(params.toString().split("=")[1]);
+        }
+
+        try(OutputStream os =httpExchange.getResponseBody()) {
+            os.write(str.getBytes());
+        }
+    }
+
+    public static void calculateHandler(HttpExchange httpExchange) throws IOException {
+        String query = httpExchange.getRequestURI().getQuery();
+        String str;
+        if (query == null) {
+            httpExchange.sendResponseHeaders(400, 0);
+            str = "a and b hasn't written";
+        } else {
+            httpExchange.sendResponseHeaders(200, 0);
+            String [] params = query.split("&");
+            String a=params[0].split("=")[1];
+            String b=params[1].split("=")[1];
+            str="result="+(Integer.parseInt(a)+Integer.parseInt(b));
+        }
+        try (OutputStream os = httpExchange.getResponseBody()) {
+            os.write(str.getBytes());
+        }
+    }
+}
